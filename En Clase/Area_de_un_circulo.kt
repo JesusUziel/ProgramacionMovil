0ca912c29d71ab66463d2dee69f8154059ecfe6e
JesusUziel <62316196+JesusUziel@users.noreply.github.com>
2023-10-03 22:42:43 -0600

    Delete En Clase directory

--- En Clase/Area_de_un_circulo.kt
+++
@@ -1,9 +0,0 @@
-
-fun main() {
-    val PI:Double = 3.1416
-    val radio = 5.0
-
-    val area = PI * radio * radio
-
-    println("El área del círculo con radio $radio unidades es: $area unidades cuadradas")
-}
